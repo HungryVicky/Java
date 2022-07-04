2d7a3a9e67cca18868fc96568a809438b7953845
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-07-04 21:58:10 +0530

    Packages In Java

---
+++ package.java
@@ -0,0 +1,14 @@
+import Tool.toolbox;
+
+public class package{
+    public static void main(String[] args) {
+toolbox box = new toolbox();
+    }
+}
+package Tool;
+
+public class toolbox {
+}
+
+
+
