1a0b0d20cc284c339840c7bbc3ad020d34f0a6b5
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-07-04 21:45:47 +0530

    JOptionPanel In Java

---
+++ guiJava.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class guiJava {
+    public static void main (String[] args){
+        String nameOfThePerson;
+        int ageOfThePerson;
+
+        nameOfThePerson = JOptionPane.showInputDialog("Enter Your Name :");
+        ageOfThePerson = Integer.parseInt(JOptionPane.showInputDialog("Enter Your Age :"));
+        JOptionPane.showMessageDialog(null,"Your Name :"+nameOfThePerson);
+        JOptionPane.showMessageDialog(null,"You Are"+ageOfThePerson+"Years Old");
+
+
+
+    }
