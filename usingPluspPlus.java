67821dcae1fc5226423b522e0f23a9ace57963ef
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-07-04 22:03:35 +0530

    usingPluspPlus In Java

---
+++ usingPluspPlus.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class usingPluspPlus {
+    public static void main(String[] args){
+
+     int friend = 10;
+     friend++;
+    System.out.Println("Using ++ Operator :"+friend);
+     friend--;
+    System.out.Println("Using -- operator :"+friend);
+
+        
+    }
+}
