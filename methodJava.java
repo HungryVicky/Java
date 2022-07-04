537d0cd47d0e19b6f6a28695bb35450dc4053fd3
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-07-04 21:53:51 +0530

    methodJava In Java

---
+++ methodJava.java
@@ -0,0 +1,14 @@
+//method in c++ know as functions or struct
+public class methodJava {
+    public static void method (String[] args){
+
+        int x = 10,y =20 ;
+        add(x,y);
+        System.out.println(add(x,y));
+
+    }
+    //using method to return the value od x and y added 
+static int add(int x, int y) {
+        return x+y;
+}
+}
