baf32f055854ff6a3828476786484918b1bc83c3
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-07-04 21:33:15 +0530

    Inter Change Two Values Of Integer In Java

---
+++ changeValue.java
@@ -0,0 +1,19 @@
+public class changeValue{
+
+    public static void main (String[] args){
+
+    String data1 = "A";
+    String data2 = "B";
+    String temp;
+
+    temp = data1;
+    data1 = data2;
+    data2 = temp;
+
+        System.out.println("data 1 ="+data1);
+        System.out.println("data 2 ="+data2);
+
+
+
+    }
+}
