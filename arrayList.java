8015f84288201c44b7a24b2dfe1f3f3c6f348985
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-07-04 21:29:32 +0530

    array-list In Java

---
+++ arrayList.java
@@ -0,0 +1,20 @@
+//header file for arraylist
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main (String[] args){
+    
+//array list declaration 
+        ArrayList<String> Arraylist = new ArrayList<>();
+//array list initialization 
+        Arraylist.add("vicky");
+        Arraylist.add("java");
+        Arraylist.add("python");
+        Arraylist.add("c++");
+        Arraylist.add("c#");
+//displaying array list
+        System.out.println(Arraylist);
+        
+    }
+
+}
