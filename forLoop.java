1a1db653f364512ca212a6c2c91861f5a6e2f859
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-07-04 21:43:45 +0530

    forLoop In Java

---
+++ forLoop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class main{
+
+  public static void main(string[] args){
+
+      int data;
+       Scanner input = new Scanner(System.in);
+        System.out.println("Enter The Number To Get Sum Of Number :");
+         data = input.nextInt();
+           for(int i = 1 ; i<= data ; i++){
+             System.out.println("The Sum Of Number :"+i);
+         }
+     }
+
+}
